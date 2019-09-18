348f0664d11637aef7e59617743c671592540424
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-09-18 08:53:41 -0700

    Added class

---
+++ BMI Calculator/ViewController2.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController2.swift
+//  BMI Calculator
+//
+//  Created by Mac Is Trash on 18/09/2019.
+//  Copyright © 2019 Mikołajczyk Grzegorz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var waga: UITextField!
+    @IBOutlet var wzrost: [UITextField]!
+    @IBOutlet weak var output: UILabel!
+    
+    @IBAction func button(_ sender: UIButton) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
